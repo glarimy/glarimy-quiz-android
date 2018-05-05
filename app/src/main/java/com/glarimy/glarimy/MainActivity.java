4cd1f737732d9be062902bb02c5976581067dc19
Krishna Mohan Koyya <krishna@glarimy.com>
2018-05-05 12:45:30 +0530

    Initial commit

---
+++ app/src/main/java/com/glarimy/glarimy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.glarimy.glarimy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void verify(View view){
+        Log.e("Glarimy", "You clicked");
+        Toast.makeText(this, "Great! You got it!", Toast.LENGTH_LONG).show();
+    }
+}
